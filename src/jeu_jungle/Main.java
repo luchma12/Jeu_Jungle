dcbdd997a7c11e9cfd20c829066973f8efcfed14
lucas <lucas@LAPTOP-OQEGRK7J>
2021-01-28 19:13:31 +0100

    création du dépot

---
+++ src/jeu_jungle/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeu_jungle;
+
+/**
+ *
+ * @author lucas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        IHMPlateau ihm = new IHMPlateau();
+        ihm.setVisible(true);
+    }
+    
+}
